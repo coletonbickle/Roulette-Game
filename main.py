894649963cec2e0f2cc048e7e63e01a24cd11d72
Coleton Bickle <91445808+coletonbickle@users.noreply.github.com>
2021-10-01 21:57:29 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+import math as math
+import numpy as np
+import random
+
+
+class Wheel:
+    def __init__(self, x=None):
+        self.Wheel = x
+
+    def val(self):
+        self.Wheel = random.randint(-1, 36)
+
+
+def main():
+    ask()
+
+
+def ask():
+    money = float(input("How Much money are you bringing to the Table? (dollar amount): $"))
+    print(f"You have brought ${money:.0f} to the table.")
+
+    def _ask2():
+        _ok = str(input("Type 'Ok' to continue or 'quit' to quit: ").lower())
+        if _ok == "ok":
+            print("\nPayouts:")
+            print("Single number bet pays 35 to 1")
+            print("Double number bet pays 17 to 1")
+            print("Three number bet pays 11 to 1")
+            print("Four number bet pays 8 to 1")
+            print("Five number bet pays 6 to 1")
+            print("Six number bet pays 5 to 1")
+            print("Dozens pays 2 to 1")
+            print("Column bets pays 2 to 1")
+            print("1-18 pays even money")
+            print("19-36 pays even money")
+            print("Red or black pays even money")
+            print("Odd or even bets pays even money\n")
+            pass
+        elif _ok == "quit":
+            _kill()
+        else:
+            _ask2()
+    _ask2()
+
+
+def _kill():
+    exit()
+
+
+if __name__ == '__main__':
+    main()
+
